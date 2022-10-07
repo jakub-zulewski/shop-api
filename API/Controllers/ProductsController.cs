014d56d27c7fb3019dd6f82e33e8448314831b52
jakub-zulewski <j.zulewski@outlook.com>
2022-10-07 18:55:56 +0200

    Add Products controller, database initializer, Postman collection.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+	private readonly StoreContext _storeContext;
+
+	public ProductsController(StoreContext storeContext)
+	{
+		_storeContext = storeContext;
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+	{
+		return Ok(await _storeContext.Products.ToListAsync());
+	}
+
+	[HttpGet("{id:int}")]
+	public async Task<ActionResult<Product>> GetProduct(int id)
+	{
+		var product = await _storeContext.Products.FindAsync(id);
+
+		if (product is null)
+			return NoContent();
+
+		return Ok(product);
+	}
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,23 +8,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("Shop API", new OpenApiInfo
-    {
-        Title = "Shop API",
-        Version = "v1"
-    });
+	options.SwaggerDoc("Shop API", new OpenApiInfo
+	{
+		Title = "Shop API",
+		Version = "v1"
+	});
+});
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/Shop%20API/swagger.json", "Shop API v1");
-        options.RoutePrefix = "api/docs";
-    });
+	app.UseSwagger();
+	app.UseSwaggerUI(options =>
+	{
+		options.SwaggerEndpoint("/swagger/Shop%20API/swagger.json", "Shop API v1");
+		options.RoutePrefix = "api/docs";
+	});
 }
 
 app.UseHttpsRedirection();
@@ -31,4 +37,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+using var scope = app.Services.CreateAsyncScope();
+try
+{
+	var storeContext = scope.ServiceProvider.GetRequiredService<StoreContext>();
+
+	await storeContext.Database.MigrateAsync();
+	await DbInitializer.InitializeDatabase(storeContext);
+}
+catch (Exception ex)
+{
+	var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+	logger.LogError(ex, "Problem migrationg data.");
+}
+
+await app.RunAsync();
