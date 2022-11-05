ee9754d343ed1e0d7a0dcbff7c1ec2c0d822d5ce
jakub-zulewski <j.zulewski@outlook.com>
2022-11-05 21:46:01 +0100

    Add search product.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -16,9 +16,9 @@ public class ProductsController : BaseAPIController
 	}
 
 	[HttpGet]
-	public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? orderBy)
+	public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? orderBy, string? searchTerm)
 	{
-		var query = _storeContext.Products.Sort(orderBy!).AsQueryable();
+		var query = _storeContext.Products.Sort(orderBy!).Search(searchTerm!).AsQueryable();
 
 		return await query.ToListAsync();
 	}
